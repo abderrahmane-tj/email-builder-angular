bbb3456076bb2ce8cfc87f7fa440a52957a12c43
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-03-08 16:26:48 +0000

    make editable content directive more generic
    instead of paragraphe editable, it is now named element editable

--- app/js/directives/element-editable.directive.js
+++ app/js/directives/element-editable.directive.js
@@ -14,21 +14,22 @@ emailApp.directive('elementEditable',['$sce', function($sce){
     };
     function link($scope, element, attrs, ngModel){
         element.bind('click', function (event) {
+            console.log('clicked on element');
             if(element.is('[contenteditable=true]')){
+                console.log('is editable, get out');
                 event.stopImmediatePropagation();
                 return;
             }
+            console.log('start editing');
             createEditor($scope, element, attrs, ngModel);
         });
     }
     function createEditor($scope, element, attrs, ngModel){
-        //element.removeAttr('ng-bind-html');
         attrs.$set('contenteditable','true');
-        //element.get(0).focus();
+        element.get(0).focus();
         var config = {
             extraPlugins: 'colorbutton,colordialog,font,justify,liststyle,indentblock',
             floatSpaceDockedOffsetY: 10,
-            startupFocus: true,
             toolbar: [
                 { name: 'basicstyles', items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
                 { name: 'links', items: [ 'Link', 'Unlink']},
@@ -39,41 +40,37 @@ emailApp.directive('elementEditable',['$sce', function($sce){
             ],
             removePlugins:'magicline'
         };
-
         var cke = CKEDITOR.inline(element.get(0),config);
-        //cke.on('pasteState', function() {
-        //    $scope.$apply(function() {
-        //        ngModel.$setViewValue(cke.getData());
-        //    });
-        //});
 
-        //element.on('change blur keyup', function() {
         // TODO: make this sync continuesly not just on blur
+        //element.on('change blur keyup', function() {
         element.on('blur', function() {
+            console.log('blurred, saving');
             ngModel.$setViewValue(cke.getData());
         });
 
         $('html,[highlight]').one('click', function (event) {
-            removeContentEditable($(event.target), element, cke);
+            console.log('clicked outside, destroying');
+            var target = $(event.target);
+            var targetIsElement = target.closest('[element-editable]')
+                    .is(element);
+            var targetIsCKE = target.parents('.cke').length;
+            if(targetIsElement || targetIsCKE){
+                console.log('either clicked on element or CKE UI. nothing to do');
+                return;
+            }
+
+            removeContentEditable(element, cke);
         });
-        $('.page').addClass('editing');
     }
-    function removeContentEditable(target, paragraphe, cke, ngModel) {
-        var targetIsElement =
-            target.closest('[paragraphe-editable]')
-                .is(paragraphe);
-        var targetIsCKE = target.parents('.cke').length;
-        // if the click was on CKE's UI, do not close
-        if(targetIsElement || targetIsCKE){
-            return;
-        }
+    function removeContentEditable(paragraphe, cke) {
 
         if(paragraphe.is(':focus')){
             paragraphe.trigger('blur');
         }
-        $('html,[highlight]').off('click', removeContentEditable);
+
         cke.destroy();
         paragraphe.removeAttr('contenteditable');
-        //paragraphe.attr('ng-bind-html','element.content');
+        $('html,[highlight]').off('click', removeContentEditable);
     }
 }]);
