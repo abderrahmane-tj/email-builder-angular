0dc02e746fe24fe1c19e153c16deda22a59c3ca3
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-03-30 12:08:55 +0100

    fix section templates : was using ink1.0's css

--- app/js/directives/layout/inspector.directive.js
+++ app/js/directives/layout/inspector.directive.js
@@ -5,9 +5,10 @@ emailApp.directive('inspector',[function(){
         replace: true,
         controllerAs: 'inspctVM',
         bindToController: true,
-        scope:{
-            element: '=currentElement'
-        },
+        scope:true,
+        //scope:{
+        //    element: '=currentElement'
+        //},
         link:function($scope,element,attrs){
         },
         controller:function($scope){
@@ -30,31 +31,31 @@ emailApp.directive('inspector',[function(){
                 }
             ];
             vm.sectionTemplate = [
-                { "template-name": '[100%]',"style": {}, "type": "section", columns: [{ "type": "column", "grid-width": "twelve", "style": {}, "elements": []}]},
+                { "template-name": '[100%]',"style": {}, "type": "section", columns: [{ "type": "column", "grid-width": "12", "style": {}, "elements": []}]},
                 { "template-name": '[ 50% | 50% ]',
                     "style": {}, "type": "section",
                     columns: [
-                        { "type": "column", "grid-width": "six", "style": {}, "elements": []},
-                        { "type": "column", "grid-width": "six", "style": {}, "elements": []}
+                        { "type": "column", "grid-width": "6", "style": {}, "elements": []},
+                        { "type": "column", "grid-width": "6", "style": {}, "elements": []}
                     ]
                 },{ "template-name": '[ 33% | 33% | 33% ]',
                     "style": {}, "type": "section",
                     columns: [
-                        { "type": "column", "grid-width": "four", "style": {}, "elements": []},
-                        { "type": "column", "grid-width": "four", "style": {}, "elements": []},
-                        { "type": "column", "grid-width": "four", "style": {}, "elements": []}
+                        { "type": "column", "grid-width": "4", "style": {}, "elements": []},
+                        { "type": "column", "grid-width": "4", "style": {}, "elements": []},
+                        { "type": "column", "grid-width": "4", "style": {}, "elements": []}
                     ]
                 },{ "template-name": '[ 33% | 60% ]',
                     "style": {}, "type": "section",
                     columns: [
-                        { "type": "column", "grid-width": "four", "style": {}, "elements": []},
-                        { "type": "column", "grid-width": "eight", "style": {}, "elements": []}
+                        { "type": "column", "grid-width": "4", "style": {}, "elements": []},
+                        { "type": "column", "grid-width": "8", "style": {}, "elements": []}
                     ]
                 },{ "template-name": '[ 60% | 30% ]',
                     "style": {}, "type": "section",
                     columns: [
-                        { "type": "column", "grid-width": "eight", "style": {}, "elements": []},
-                        { "type": "column", "grid-width": "four", "style": {}, "elements": []}
+                        { "type": "column", "grid-width": "8", "style": {}, "elements": []},
+                        { "type": "column", "grid-width": "4", "style": {}, "elements": []}
                     ]
                 }
             ];
