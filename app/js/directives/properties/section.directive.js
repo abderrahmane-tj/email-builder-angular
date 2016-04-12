9dd1e14cddf39e089042b148d63a763accaab75a
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-04-12 12:35:45 +0100

    add section properties

---
+++ app/js/directives/properties/section.directive.js
@@ -0,0 +1,10 @@
+var emailApp = angular.module('emailApp');
+emailApp.directive('sectionProperties',function(){
+    return {
+        restrict: "E",
+        scope:{
+            section:'=element'
+        },
+        templateUrl: 'app/templates/properties/section.template.html'
+    }
+});
