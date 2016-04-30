01060a389ad0e0872432358f3da5067357f67113
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-04-30 13:24:46 +0100

    add section background

--- app/js/directives/properties/section.directive.js
+++ app/js/directives/properties/section.directive.js
@@ -5,6 +5,13 @@ emailApp.directive('sectionProperties',function(){
         scope:{
             section:'=element'
         },
-        templateUrl: 'app/templates/properties/section.template.html'
+        templateUrl: 'app/templates/properties/section.template.html',
+        link: link
+    };
+    function link($scope, element, attrs){
+        $scope.$watch("section.wrapper.rawStyle['background-image']", function (url) {
+            console.log(url);
+            //$scope.section.wrapper.style['background-image'] = "url('"+url+"')";
+        })
     }
 });
