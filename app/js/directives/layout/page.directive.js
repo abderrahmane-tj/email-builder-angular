2833a5b2787bfac9d0f9a6add12ba622db8723cd
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-05-04 13:19:02 +0100

    move page logic to page.directive from mainController
    also don't start building page until data is loaded. done through ng-if on app body

--- app/js/directives/layout/page.directive.js
+++ app/js/directives/layout/page.directive.js
@@ -1,16 +1,145 @@
 var emailApp = angular.module('emailApp');
-emailApp.directive('page',function(){
+emailApp.directive('page',[
+    'dragulaService','$timeout','repositionTooltip',
+    pageDirective
+]);
+
+function pageDirective(dragulaService,$timeout,repositionTooltip){
     return {
         restrict: "E",
         replace: true,
         controllerAs: 'pageVM',
-        controller: function($scope){
-            var vm = this;
-            vm.assignElement = function (element) {
-                $scope.mainVM.currentElement = element;
-                $scope.$apply();
-            };
-        },
+        controller: controller,
+        link:link,
         templateUrl: 'app/templates/page.template.html'
+    };
+    function controller($scope){
+        var vm = this;
+        vm.assignElement = assignElement;
+        handleDragAndDrop();
+        handleAutoScroll();
+        handlePageScroll();
+        /////////////////////
+        function assignElement(element){
+            $scope.mainVM.currentElement = element;
+            $scope.$apply();
+        }
+        function handleDragAndDrop(){
+            dragulaService.options($scope, 'sections-bag', sectionsBagConfig());
+            dragulaService.options($scope, 'elements-bag', elementsBagConfig());
+            $scope.$on('elements-bag.drag', handleDrag);
+            $scope.$on('elements-bag.over', handleOver);
+            $scope.$on('elements-bag.drop', handleDrop);
+            $scope.$on('sections-bag.drop', handleDrop);
+        }
+        function sectionsBagConfig(){
+            return {
+                copy: function (el, source) {
+                    return $(source).hasClass('new-section');
+                },
+                accepts: function (el, source) {
+                    return !$(source).hasClass('new-section');
+                },
+                moves: function(el, source, handle, sibling){
+                    var $handle = $(handle);
+                    var dragginTemplate = $handle.parents('.section-template').length > 0;
+                    return $handle.is('.container-wrapper,.section-template') || dragginTemplate;
+                }
+            };
+        }
+        function elementsBagConfig(){
+            return {
+                copy: function (el, source) {
+                    return $(source).hasClass('new-elements');
+                },
+                accepts: function(el, target, source, sibling){
+                    return !$(target).hasClass('new-elements');
+                },
+                moves: function (el, source, handle, sibling) {
+                    var $handle = $(handle);
+                    var hasReasonsToDrag =
+                        !$(el).find('[contenteditable]').length
+                        && !$handle.is('.resize-handle')
+                        && ($handle.parents('.resize-handle').length === 0);
+                    return hasReasonsToDrag;
+                }
+            };
+        }
+        function handleDrag(dragulaEvent,el,source){
+            var $sections = $('.section');
+            $('.column-cell',$sections).height(0);
+            $sections.each(function (i,section) {
+                var columns = $(this).find('.column-cell');
+                //if(!columns.length || columns.length === 1){
+                //    return;
+                //}
+                var heights = columns.map(function () {
+                    return $(this).height();
+                }).get();
+
+                var maxHeight = Math.max.apply(this,heights);
+                columns.height(maxHeight);
+            });
+        }
+        function handleOver(){ }
+        function handleDrop(dragulaEvent,el,target,source,sibling){
+            $timeout(repositionTooltip);
+            $('.column-cell').height(0);
+
+            if(dragulaEvent.name === "sections-bag.drop"){
+                return;
+            }
+
+            var dragged = $(el);
+            var $element = $(dragged.children().get(0));
+            var $source = $(source);
+
+            var shouldRecreateTooltip =
+                !$source.is(target) // linking occurs only when dropping outside of local bag
+                && !$source.is('.new-elements,.new-section') // we do not want to do this to new elements
+                && $element.is('.tooltipstered'); // only recrate tooltip, if existed before dragging
+            if(shouldRecreateTooltip){
+                $scope.$apply(function () {
+                    $element.scope().element.tooltipstered = true;
+                });
+            }
+
+        }
+        function handleAutoScroll(){
+            var elementsBag = dragulaService.find($scope,"elements-bag");
+            var sectionsBag = dragulaService.find($scope,"sections-bag");
+            var scroll = autoScroll([
+                document.querySelector('.autoscroll-me-please')
+            ],{
+                margin: 30,
+                pixels: 15,
+                scrollWhenOutside: true,
+                autoScroll: function(){
+                    return this.down && (
+                            elementsBag.drake.dragging
+                            || sectionsBag.drake.dragging
+                        );
+                }
+            });
+
+        }
+        function handlePageScroll(){
+            var timeout = undefined;
+            $('.page-flex').bind('scroll',function(){
+                if(timeout){
+                    $timeout.cancel(timeout);
+                }else{
+                    $(document.body).addClass('no-tooltip');
+                }
+                timeout = $timeout(function () {
+                    repositionTooltip();
+                    $(document.body).removeClass('no-tooltip');
+                    timeout = undefined;
+                },250);
+            });
+        }
     }
-});
+    function link(){
+
+    }
+}
--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,8 @@ var emailApp = angular.module('emailApp', [
     angularDragula(angular)
 ]);
 emailApp.config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.debugInfoEnabled(false);
+    // todo: disable debug. ps: disabling it will break getting scope from html. exemple : ($element.scope().element.tooltipstered = true;)
+    //$compileProvider.debugInfoEnabled(false);
 }]);
 emailApp.controller('MainController', [
     '$scope', '$templateCache', 'dragulaService', '$timeout', '$interval',
@@ -51,12 +52,6 @@ function MainController($scope, $templateCache, dragulaService, $timeout,
 
             // watch page for changes
             unbind = $scope.$watch('mainVM.page', syncData, true);
-
-            handleDragAndDrop();
-            $timeout(function () {
-                handleAutoScroll();
-                handlePageScroll();
-            });
         });
     }
     function getData(){
@@ -91,120 +86,6 @@ function MainController($scope, $templateCache, dragulaService, $timeout,
         timeoutWatch = $timeout(function () {
             save();
         }, 2000);
-    }
-    function handleDragAndDrop(){
-        dragulaService.options($scope, 'sections-bag', sectionsBagConfig());
-        dragulaService.options($scope, 'elements-bag', elementsBagConfig());
-        $scope.$on('elements-bag.drag', handleDrag);
-        $scope.$on('elements-bag.over', handleOver);
-        $scope.$on('elements-bag.drop', handleDrop);
-        $scope.$on('sections-bag.drop', handleDrop);
-    }
-    function sectionsBagConfig(){
-        return {
-            copy: function (el, source) {
-                return $(source).hasClass('new-section');
-            },
-            accepts: function (el, source) {
-                return !$(source).hasClass('new-section');
-            },
-            moves: function(el, source, handle, sibling){
-                var $handle = $(handle);
-                var dragginTemplate = $handle.parents('.section-template').length > 0;
-                return $handle.is('.container-wrapper,.section-template') || dragginTemplate;
-            }
-        };
-    }
-    function elementsBagConfig(){
-        return {
-            copy: function (el, source) {
-                return $(source).hasClass('new-elements');
-            },
-            accepts: function(el, target, source, sibling){
-                return !$(target).hasClass('new-elements');
-            },
-            moves: function (el, source, handle, sibling) {
-                var $handle = $(handle);
-                var hasReasonsToDrag =
-                    !$(el).find('[contenteditable]').length
-                    && !$handle.is('.resize-handle')
-                    && ($handle.parents('.resize-handle').length === 0);
-                return hasReasonsToDrag;
-            }
-        };
-    }
-    function handleDrag(dragulaEvent,el,source){
-        var $sections = $('.section');
-        $('.column-cell',$sections).height(0);
-        $sections.each(function (i,section) {
-           var columns = $(this).find('.column-cell');
-            //if(!columns.length || columns.length === 1){
-            //    return;
-            //}
-            var heights = columns.map(function () {
-                return $(this).height();
-            }).get();
-
-            var maxHeight = Math.max.apply(this,heights);
-            columns.height(maxHeight);
-        });
-    }
-    function handleOver(){ }
-    function handleDrop(dragulaEvent,el,target,source,sibling){
-        $timeout(repositionTooltip);
-        $('.column-cell').height(0);
-
-        if(dragulaEvent.name === "sections-bag.drop"){
-            return;
-        }
-
-        var dragged = $(el);
-        var $element = $(dragged.children().get(0));
-        var $source = $(source);
-
-        var shouldRecreateTooltip =
-            !$source.is(target) // linking occurs only when dropping outside of local bag
-            && !$source.is('.new-elements,.new-section') // we do not want to do this to new elements
-            && $element.is('.tooltipstered'); // only recrate tooltip, if existed before dragging
-        if(shouldRecreateTooltip){
-            $scope.$apply(function () {
-                $element.scope().element.tooltipstered = true;
-            });
-        }
-
-    }
-    function handleAutoScroll(){
-        var elementsBag = dragulaService.find($scope,"elements-bag");
-        var sectionsBag = dragulaService.find($scope,"sections-bag");
-        var scroll = autoScroll([
-            document.querySelector('.autoscroll-me-please')
-        ],{
-            margin: 30,
-            pixels: 15,
-            scrollWhenOutside: true,
-            autoScroll: function(){
-                return this.down && (
-                        elementsBag.drake.dragging
-                        || sectionsBag.drake.dragging
-                    );
-            }
-        });
-
-    }
-    function handlePageScroll(){
-        var timeout = undefined;
-        $('.page-flex').bind('scroll',function(){
-            if(timeout){
-                $timeout.cancel(timeout);
-            }else{
-                $(document.body).addClass('no-tooltip');
-            }
-            timeout = $timeout(function () {
-                repositionTooltip();
-                $(document.body).removeClass('no-tooltip');
-                timeout = undefined;
-            },250);
-        });
     }
     function resetData() {
         unbind();
