4d23f20d8c1ac785f37716dae8950fd46b9f0f15
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-05-05 12:26:25 +0100

    add color picker

---
+++ app/js/directives/helpers/color-picker.directive.js
@@ -0,0 +1,25 @@
+var emailApp = angular.module('emailApp');
+emailApp.directive('colorPicker',[colorPickerDirective]);
+function colorPickerDirective($compile,$timeout,$templateRequest,repositionTooltip,preventBubbling){
+    return {
+        restrict: "A",
+        scope:true,
+        controller:controller,
+        link: link
+    };
+
+    function link($scope, element, attrs){
+        $scope.$watch(attrs.ngModel, function (val) {
+            element.spectrum('set',val);
+        });
+        element.spectrum({
+            allowEmpty: true,
+            preferredFormat: 'hex',
+            showInput: true,
+            showInitial: true
+        }).show();
+    }
+
+    function controller($scope){
+    }
+}
