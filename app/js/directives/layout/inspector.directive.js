36e72671e1c2f66e3e515edf762ffc35f9121f6a
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-06-02 10:39:23 +0100

    move code to own link function

--- app/js/directives/layout/inspector.directive.js
+++ app/js/directives/layout/inspector.directive.js
@@ -6,48 +6,50 @@ emailApp.directive('inspector',['$timeout',function($timeout){
         controllerAs: 'inspctVM',
         bindToController: true,
         scope:true,
-        link:function($scope,element,attrs){ },
+        link: link,
         controller:function($scope){
-            var vm = this;
-            $timeout(function () {
-                handleTabs();
-                handleExport();
-            });
-            ///////////
-            function handleTabs(){
-                var tabsContainer = $('.tabs');
-                var active = tabsContainer.find('.active');
-                var inspectorPanels = $('.inspector-panel');
-                activateTab(active);
-                var tabs = tabsContainer.find('a');
-                tabs.bind('click', onClick);
-
-                function onClick(event) {
-                    event.preventDefault();
-                    tabsContainer.find('.active').removeClass('active');
-                    activateTab($(this));
-                }
-                function activateTab(anchor){
-                    anchor.addClass('active');
-                    inspectorPanels.hide();
-                    $(anchor.attr('href')).show();
-                }
-            }
-            function handleExport(){
-                $("#data-dump").on('focus',function() {
-                    console.log('focused');
-                    var $this = $(this);
-                    $this.select();
-
-                    // Work around Chrome's little problem
-                    $this.mouseup(function() {
-                        // Prevent further mouseup intervention
-                        $this.unbind("mouseup");
-                        return false;
-                    });
-                });
-            }
         },
         templateUrl: 'app/templates/inspector.template.html'
+    };
+    function link($scope,element,attrs){
+        $timeout(function () {
+            handleTabs();
+            handleExport();
+        });
+        ///////////
+        function handleTabs(){
+            var tabsContainer = $('.tabs');
+            var active = tabsContainer.find('.active');
+            var inspectorPanels = $('.inspector-panel');
+            activateTab(active);
+            var tabs = tabsContainer.find('a');
+            tabs.bind('click', onClick);
+
+            function onClick(event) {
+                event.preventDefault();
+                tabsContainer.find('.active').removeClass('active');
+                activateTab($(this));
+            }
+            function activateTab(anchor){
+                anchor.addClass('active');
+                inspectorPanels.hide();
+                $(anchor.attr('href')).show();
+            }
+        }
+        function handleExport(){
+            console.log($("#data-dump").length);
+            $("#data-dump").on('focus',function() {
+                console.log('focused');
+                var $this = $(this);
+                $this.select();
+
+                // Work around Chrome's little problem
+                $this.mouseup(function() {
+                    // Prevent further mouseup intervention
+                    $this.unbind("mouseup");
+                    return false;
+                });
+            });
+        }
     }
 }]);
