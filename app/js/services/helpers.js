ba3d3426bfb647040c07dc7981e3e1ca5a6a5e00
Abderrahmane TAHRI JOUTI <tj.abderrahmane@gmail.com>
2016-06-02 12:37:09 +0100

    add service to select on focus

--- app/js/services/helpers.js
+++ app/js/services/helpers.js
@@ -52,3 +52,19 @@ emailServices.factory('obj2css',function(){
         return css;
     };
 });
+
+emailServices.factory('jqSelectOnFocus',function(){
+    return function ($element) {
+        $element.on('focus',function() {
+            var $this = $(this);
+            $this.select();
+
+            // Work around Chrome's little problem
+            $this.mouseup(function() {
+                // Prevent further mouseup intervention
+                $this.unbind("mouseup");
+                return false;
+            });
+        });
+    };
+});
